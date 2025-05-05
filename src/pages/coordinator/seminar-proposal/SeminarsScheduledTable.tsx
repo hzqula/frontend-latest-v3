d644c96c25e42cd387fa94b01351eec9a6596b54
hzqula <ilooqstrasi@gmail.com>
2025-05-05 07:59:52 +0700

    🎓 add sorting to (time) column

--- src/pages/coordinator/seminar-proposal/SeminarsScheduledTable.tsx
+++ src/pages/coordinator/seminar-proposal/SeminarsScheduledTable.tsx
@@ -1,14 +1,9 @@
 "use client";
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "../../../components/ui/card";
+import { useState } from "react";
 import { Button } from "../../../components/ui/button";
 import { toast } from "react-toastify";
+import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react";
 
 interface SeminarsScheduledTableProps {
   seminars: any[];
@@ -23,99 +18,128 @@ const SeminarsScheduledTable = ({
   formatTime,
   onViewDetails,
 }: SeminarsScheduledTableProps) => {
-  console.log(seminars);
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc" | null>(
+    null
+  );
+
+  const handleSort = () => {
+    const newDirection =
+      sortDirection === "asc"
+        ? "desc"
+        : sortDirection === "desc"
+        ? null
+        : "asc";
+    setSortDirection(newDirection);
+  };
+
+  const sortedSeminars = [...seminars].sort((a, b) => {
+    const dateA = new Date(a.time);
+    const dateB = new Date(b.time);
+    if (sortDirection === "asc") {
+      return dateA.getTime() - dateB.getTime();
+    } else if (sortDirection === "desc") {
+      return dateB.getTime() - dateA.getTime();
+    }
+    return 0;
+  });
 
   return (
-    <Card className="bg-white">
-      <CardHeader>
-        <div className="flex flex-col md:flex-row justify-between md:items-center gap-4">
-          <div>
-            <CardTitle className="text-2xl -mb-1 font-heading font-black text-primary-800">
-              Seminar Proposal Dijadwalkan
-            </CardTitle>
-            <CardDescription className="text-primary">
-              Seminar proposal yang telah dijadwalkan
-            </CardDescription>
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <div className="rounded-sm overflow-x-auto border border-primary">
-          <table className="w-full text-left">
-            <thead>
-              <tr className="bg-primary text-primary-foreground font-heading font-medium">
-                <th className="p-4 w-[35%]">Judul Penelitian</th>
-                <th className="p-4 w-[20%]">Mahasiswa</th>
-                <th className="p-4 w-[20%]">Jadwal Seminar</th>
-                <th className="p-4 w-[10%]">Tempat</th>
-                <th className="p-4 w-[15%] text-center">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              {seminars.length > 0 ? (
-                seminars.map((seminar: any) => (
-                  <tr
-                    key={seminar.id}
-                    className="border-b border-primary-200 text-primary-800"
+    <div className="rounded-sm overflow-x-auto border border-primary">
+      <table className="w-full text-left">
+        <thead>
+          <tr className="bg-primary text-primary-foreground font-heading font-medium">
+            <th className="p-4 w-[1%]">No.</th>
+            <th className="px-2 py-4 w-[40%]">Judul Penelitian</th>
+            <th className="px-2 py-4 w-[25%]">Mahasiswa</th>
+            <th className="p-4 w-[10%] ">
+              <div className="flex justify-center items-center gap-1">
+                Jadwal
+                <button
+                  onClick={handleSort}
+                  className="focus:outline-none hover:text-primary-200"
+                >
+                  {sortDirection === "asc" && <ArrowUp className="w-4 h-4" />}
+                  {sortDirection === "desc" && (
+                    <ArrowDown className="w-4 h-4" />
+                  )}
+                  {sortDirection === null && (
+                    <ArrowUpDown className="w-4 h-4" />
+                  )}
+                </button>
+              </div>
+            </th>
+            <th className="p-4 w-[10%] text-center">Tempat</th>
+            <th className="p-4 w-[14%] text-center">Aksi</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedSeminars.length > 0 ? (
+            sortedSeminars.map((seminar: any, index: number) => (
+              <tr
+                key={seminar.id}
+                className="border-b bg-white border-primary-200 text-primary-800"
+              >
+                <td className="p-2">
+                  <div className="font-medium text-center text-primary-800">
+                    {index + 1}
+                  </div>
+                </td>
+                <td className="p-2">
+                  <div className="font-medium">{seminar.title}</div>
+                </td>
+                <td className="p-2">
+                  <div className="font-medium text-primary-800">
+                    {seminar.student?.name || "N/A"}
+                  </div>
+                  <div className="text-sm text-primary">
+                    {seminar.studentNIM}
+                  </div>
+                </td>
+                <td className="p-2 flex flex-col justify-center items-center">
+                  <div className="text-center">{formatDate(seminar.time)}</div>
+                  <div className="text-sm text-center text-primary">
+                    {formatTime(seminar.time)}
+                  </div>
+                </td>
+                <td className="p-2 text-center">{seminar.room || "TBD"}</td>
+                <td className="p-2 text-center space-x-2">
+                  <Button
+                    variant="outline"
+                    size="sm"
+                    className="border-primary-400 text-primary-700"
+                    onClick={() => onViewDetails(seminar)}
                   >
-                    <td className="p-4">
-                      <div className="font-medium">{seminar.title}</div>
-                    </td>
-                    <td className="p-4">
-                      <div className="font-medium text-primary-800">
-                        {seminar.student?.name || "N/A"}
-                      </div>
-                      <div className="text-sm text-primary-600">
-                        {seminar.studentNIM}
-                      </div>
-                    </td>
-                    <td className="p-4">
-                      <div>{formatDate(seminar.time)}</div>
-                      <div className="text-sm text-primary-600">
-                        {formatTime(seminar.time)}
-                      </div>
-                    </td>
-                    <td className="p-4">{seminar.room || "TBD"}</td>
-                    <td className="p-4 text-center space-x-2">
-                      <Button
-                        variant="outline"
-                        size="sm"
-                        className="border-primary-400 text-primary-700"
-                        onClick={() => onViewDetails(seminar)}
-                      >
-                        Lihat
-                      </Button>
-                      <Button
-                        size="sm"
-                        className="bg-primary-600 text-white hover:bg-primary-700"
-                        onClick={() => {
-                          if (seminar.folderId) {
-                            window.open(
-                              `https://drive.google.com/drive/u/4/folders/${seminar.folderId}`,
-                              "_blank"
-                            );
-                          } else {
-                            toast.error("Link Google Drive tidak tersedia.");
-                          }
-                        }}
-                      >
-                        Folder
-                      </Button>
-                    </td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan={5} className="p-4 text-center text-primary-600">
-                    Tidak ada seminar proposal yang dijadwalkan ditemukan.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </CardContent>
-    </Card>
+                    Lihat
+                  </Button>
+                  <Button
+                    size="sm"
+                    className="bg-primary-600 text-white hover:bg-primary-700"
+                    onClick={() => {
+                      if (seminar.folderId) {
+                        window.open(
+                          `https://drive.google.com/drive/u/4/folders/${seminar.folderId}`,
+                          "_blank"
+                        );
+                      } else {
+                        toast.error("Link Google Drive tidak tersedia.");
+                      }
+                    }}
+                  >
+                    Folder
+                  </Button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={6} className="p-4 text-center text-primary-600">
+                Tidak ada seminar proposal yang dijadwalkan ditemukan.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
